6a1b668c6c0cc2632584cba1ca9f923bbf7944be
Văn quỳnh <vanquynh140498@gmail.com>
2026-06-08 22:05:36 +0700

    sửa

---
+++ web_luat/Areas/Admin/Controllers/QuyTrinhDuyetBaiController.cs
@@ -0,0 +1,118 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using web_luat.Models;
+
+namespace web_luat.Areas.Admin.Controllers
+{
+    [AdminAuthorize]
+    public class QuyTrinhDuyetBaiController : Controller
+    {
+        dbcontext db = new dbcontext();
+        public ActionResult Index(int? page, string Keyword = "")
+        {
+            ViewData["nhomquyen"] = db.tbl_NhomQuyen.ToList();
+            var list = db.QuyTrinhDuyets.AsQueryable().OrderBy(g => g.STT);
+            if (page == null) page = 1;
+            var books = list.OrderBy(g => g.Id);
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            ViewBag.pageNumber = page;
+            ViewBag.total = list.ToList().Count();
+            ViewBag.key = Keyword;
+            return View(books.ToPagedList(pageNumber, pageSize));
+        }
+
+        public PartialViewResult Create()
+        {
+            ViewData["nhomquyen"] = db.tbl_NhomQuyen.ToList();
+            return PartialView();
+        }
+        [HttpPost]
+        public JsonResult Create([Bind(Include = "Id,STT,IdNhomQuyen,TenBuoc")] QuyTrinhDuyet banner)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(banner.TenBuoc))
+                {
+                    return Json(new { status = false, message = "Không được để trống các dữ liệu bắt buộc." }, JsonRequestBehavior.AllowGet);
+
+                }
+                db.QuyTrinhDuyets.Add(banner);
+                db.SaveChanges();
+                tbl_Log a = new tbl_Log();
+                a.MoTa = "Thêm mới quy trình duyệt";
+                a.Controller = "QuyTrinhDuyet";
+                a.Action = "Create";
+                a.NgayThucHien = DateTime.Now;
+                a.IdUser = Convert.ToInt32(Session["Id"].ToString());
+                db.tbl_Log.Add(a);
+                db.SaveChanges();
+                return Json(new { status = true, message = "Thêm mới thành công" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { status = true, message = "Lỗi dữ liệu" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        // GET: Admin/Banners/Edit/5
+        public PartialViewResult Edit(int? id)
+        {
+            ViewData["nhomquyen"] = db.tbl_NhomQuyen.ToList();
+
+            QuyTrinhDuyet banner = db.QuyTrinhDuyets.Find(id);
+            return PartialView(banner);
+        }
+        [HttpPost]
+        public JsonResult Edit([Bind(Include = "Id,STT,IdNhomQuyen,TenBuoc")] QuyTrinhDuyet banner)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(banner.TenBuoc))
+                {
+                    return Json(new { status = false, message = "Không được để trống các dữ liệu bắt buộc." }, JsonRequestBehavior.AllowGet);
+
+                }
+                db.QuyTrinhDuyets.AddOrUpdate(banner);
+                db.SaveChanges();
+                tbl_Log a = new tbl_Log();
+                a.MoTa = "Sửa quy trình duyệt";
+                a.Controller = "QuyTrinhDuyet";
+                a.Action = "Edit";
+                a.NgayThucHien = DateTime.Now;
+                a.IdUser = Convert.ToInt32(Session["Id"].ToString());
+                db.tbl_Log.Add(a);
+                db.SaveChanges();
+                return Json(new { status = true, message = "Cập nhật dữ liệu thành công" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { status = true, message = "Lỗi dữ liệu" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public JsonResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return Json(new { status = true, message = "Chuyên mục không tồn tại." }, JsonRequestBehavior.AllowGet);
+            }
+            QuyTrinhDuyet banner = db.QuyTrinhDuyets.Find(id);
+            db.QuyTrinhDuyets.Remove(banner);
+            db.SaveChanges();
+            tbl_Log a = new tbl_Log();
+            a.MoTa = "Xóa quy trình duyệt ";
+            a.Controller = "QuyTrinhDuyet";
+            a.Action = "Delete";
+            a.NgayThucHien = DateTime.Now;
+            a.IdUser = Convert.ToInt32(Session["Id"].ToString());
+            db.tbl_Log.Add(a);
+            db.SaveChanges();
+            return Json(new { status = true, message = "Xóa thành công." }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
